7efe670447bb6ac1851fd5df5272e639fe41dfec
Zulfiya <zulya.h95@mail.ru>
2021-08-15 22:58:41 +0300

    Return theme onload

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -12,19 +12,6 @@
                 theme == 'light' ? 'bi-lightbulb-fill.svg' : 'bi-lightbulb.svg'
               "
             />
-            <!-- <img alt="bulb" class="bulb" src="bi-lightbulb-fill.svg" /> -->
-            <!-- <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-lightbulb-fill"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z"
-              />
-            </svg> -->
           </button>
           <TodoItem :dayName="'sat'" />
         </div>
@@ -49,7 +36,7 @@ import TodoItem from './TodoItem'
 export default {
   data() {
     return {
-      theme: 'light'
+      theme: null
     }
   },
   components: {
@@ -58,19 +45,15 @@ export default {
   methods: {
     toggleStyles() {
       this.theme = this.theme == 'light' ? 'dark' : 'light'
-      // let mainTag = document.querySelector('.main-tag')
-      // mainTag.classList.toggle('dark-theme')
+    },
+    setThemeOnLoad() {
+      let time = new Date().getHours()
+      time > 6 && time < 21 ? (this.theme = 'light') : (this.theme = 'dark')
     }
-    // BulbIcon() {
-    //   let mainTag = document
-    //     .querySelector('.main-tag')
-    //     .classList.contains('dark-theme')
-    //   mainTag ? 'bi-lightbulb.svg' : 'bi-lightbulb-fill.svg'
-
-    //   console.log(mainTag)
-    // }
+  },
+  mounted() {
+    this.setThemeOnLoad()
   }
-  // computed: {}
 }
 </script>
 
